f9da5bb7362d05c9387af294a82a536e8009f04e
Fredrik Andersson <fredrik.andersson2@apotekhjartat.se>
2021-02-21 16:35:51 +0100

    added cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import path from "path";
+import cookieParser from 'cookie-parser'
 const app = express();
 const port = 8080; // default port to listen
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
+
+app.use(cookieParser());
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
